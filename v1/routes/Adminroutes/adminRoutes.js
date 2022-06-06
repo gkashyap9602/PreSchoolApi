8ff1ba3e71328ef11623e8822894d6536d519f43
gkashyap9602 <gkashyap9602@gmail.com>
2022-06-06 18:19:22 +0530

    11th

--- v1/routes/Adminroutes/adminRoutes.js
+++ v1/routes/Adminroutes/adminRoutes.js
@@ -6,23 +6,23 @@ const { FileUpload } = require('../../../Services/index')
 
 router.get('/helloadmin',Controller.AdminController.helloadmin)
 
-router.post('/create/user',FileUpload.multerAdmin.single('student_img'),Controller.AdminController.Newstd_Userfun)
-router.patch('/update/user/:id',Controller.AdminController.Update_userfun)
-router.delete('/delete/user/:id',Controller.AdminController.Delete_Userfun)
+router.post('/user/create',FileUpload.multerAdmin.single('student_img'),Controller.AdminController.Newstd_Userfun)
+router.patch('/user/update/:id',Controller.AdminController.Update_userfun)
+router.delete('/user/delete/:id',Controller.AdminController.Delete_Userfun)
 
-router.post('/create/class',FileUpload.multerAdmin.single('img'),Controller.AdminController.Add_Classfun)
-router.patch('/update/class/:id',Controller.AdminController.Update_classfun)
-router.delete('/delete/class/:id',Controller.AdminController.Delete_classfun)
+router.post('/class/create',FileUpload.multerAdmin.single('img'),Controller.AdminController.Add_Classfun)
+router.patch('/class/update/:id',Controller.AdminController.Update_classfun)
+router.delete('/class/delete/:id',Controller.AdminController.Delete_classfun)
 
-router.post('/create/student',FileUpload.multerAdmin.single('img'),Controller.AdminController.Student_addfun)
+router.post('/student/create',FileUpload.multerAdmin.single('img'),Controller.AdminController.Student_addfun)
 
-router.get('/getusers',Controller.AdminController.User_detailsfun)
-router.get('/getoneuser',Controller.AdminController.SingleUserDetail)
+router.get('/users',Controller.AdminController.User_detailsfun)
+router.get('/oneuser',Controller.AdminController.SingleUserDetail)
 
-router.get('/getclasses',Controller.AdminController.get_classes)
-router.get('/get/oneclassdata',Controller.AdminController.AllStudentOfOneClass)
+router.get('/classes',Controller.AdminController.get_classes)
+router.get('/oneclassdata',Controller.AdminController.AllStudentOfOneClass)
 
-router.post('/create/transaction',Controller.AdminController.Trans_historyfun)
+router.post('/transaction/create',Controller.AdminController.Trans_historyfun)
 
 
 
